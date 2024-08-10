d1f921483d109b8eac26a29e6a19e7e7b2009cde
Y <y4kenz1@protonmail.com>
2024-08-10 12:01:56 +0200

    Project Initialization

---
+++ src/main/java/y4kenz1/locationsystem/LocationSystemApplication.java
@@ -0,0 +1,13 @@
+package y4kenz1.locationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/y4kenz1/locationsystem/LocationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package y4kenz1.locationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
